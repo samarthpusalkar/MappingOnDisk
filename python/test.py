bdb78d30c7aa36f7b5b4538d245d7ee5ff6a6a9b
samarthpusalkar <68862617+samarthpusalkar@users.noreply.github.com>
2022-06-11 01:53:11 +0530

    test case for shrink

--- python/test.py
+++ python/test.py
@@ -1,13 +1,19 @@
 from diskMap import DiskMap
-
+import os
 if __name__=="__main__":
     dm = DiskMap(".","HashMapName")
     for i in range(8000):
         print(i)
         dm.add(str(i),i)
-    for i in range(600):
+    for i in range(5000,8000):
         dm.delete(str(i))
-    for i in range(7000,8000):
+    for i in range(200):
+        dm.delete(str(i))
+    for i in range(8000,9000):
         dm.add(str(i),i+1000)
     for i in range(8000):
         print(dm.read(str(i)))
+    print("size before shrink ", os.path.getsize(self.filePath)/BLOCK_SIZE)
+    dm.shrink()
+    print("size after shrink", os.path.getsize(self.filePath)/BLOCK_SIZE)
+    dm.close()
