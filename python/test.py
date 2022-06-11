4a0fa44a34d1c7154b9100e6ac51dd3979876bab
samarthpusalkar <68862617+samarthpusalkar@users.noreply.github.com>
2022-06-11 18:24:56 +0530

    test case improved

--- python/test.py
+++ python/test.py
@@ -5,15 +5,26 @@ if __name__=="__main__":
     dm = DiskMap(".","HashMapName")
     for i in range(8000):
         print(i)
-        dm.add(str(i),i)
+        if str(i) not in dm.add(str(i),i):
+            print("Failed at, ",i)
+            raise "Addition Failed"
+    for i in range(8000):
+        if str(i) not in dm.read(str(i)):
+            raise "Retrival Failed"
     for i in range(5000,8000):
         dm.delete(str(i))
+    for i in range(5000,8000):
+        if dm.read(str(i))!=None:
+            raise "Delete Failed"
     for i in range(50):
         dm.delete(str(i))
-    for i in range(200):
-        dm.delete(str(i))
+    for i in range(50):
+        if dm.read(str(i))!=None:
+            raise "Delete Failed"
     for i in range(8000,9000):
-        dm.add(str(i),i+1000)
+        if str(i+1000) not in dm.add(str(i),i+1000):
+            print("Failed at, ",i)
+            raise "Addition Failed"
     for i in range(8000):
         print(dm.read(str(i)))
     print("size before shrink ", os.path.getsize(dm.filePath)/BLOCK_SIZE)
