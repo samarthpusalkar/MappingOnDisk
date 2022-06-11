f3f1b74d9d2303b0fcabea449546d7f83b7fdd55
samarthpusalkar <68862617+samarthpusalkar@users.noreply.github.com>
2022-06-11 18:19:54 +0530

    test case fix

--- python/test.py
+++ python/test.py
@@ -1,5 +1,6 @@
 from diskMap import DiskMap
 import os
+from diskMap import BLOCK_SIZE
 if __name__=="__main__":
     dm = DiskMap(".","HashMapName")
     for i in range(8000):
@@ -7,13 +8,15 @@ if __name__=="__main__":
         dm.add(str(i),i)
     for i in range(5000,8000):
         dm.delete(str(i))
+    for i in range(50):
+        dm.delete(str(i))
     for i in range(200):
         dm.delete(str(i))
     for i in range(8000,9000):
         dm.add(str(i),i+1000)
     for i in range(8000):
         print(dm.read(str(i)))
-    print("size before shrink ", os.path.getsize(self.filePath)/BLOCK_SIZE)
+    print("size before shrink ", os.path.getsize(dm.filePath)/BLOCK_SIZE)
     dm.shrink()
-    print("size after shrink", os.path.getsize(self.filePath)/BLOCK_SIZE)
+    print("size after shrink", os.path.getsize(dm.filePath)/BLOCK_SIZE)
     dm.close()
